546c2a4f435e8e082d9a0a06f55b664f4b63147e
Germán Pla <germanps@outlook.es>
2018-06-04 15:43:03 +0200

    OnChange event cantidad

--- src/components/Ordenar/Ordenar.js
+++ src/components/Ordenar/Ordenar.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './Ordenar.css';
 
 class Ordenar extends Component{
+    actualizarCantidad(e){
+        console.log(e.target.value);
+    }
     render(){
         return(
             <div className="ordenar-inputs">
                 <div className="ordenar-inputs-cantidad">
                     <label> Cantidad: </label>
-                    <input type="number" min='0' max='20' placeholder="0"/>
+                    <input onChange={this.actualizarCantidad.bind(this)} type="number" min='0' max='20' placeholder="0"/>
                 </div>
                 
                 <label className="ordenar-inputs-precio"> Precio: {this.props.precio}</label>
