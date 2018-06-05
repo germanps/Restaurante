a0264a6125f68f0c99359b92964dc8d753554f89
germanps <gpla@pasiona.com>
2018-06-05 09:25:18 +0200

    Mostrar pedidos > 0

--- src/components/Pedidos/Pedidos.js
+++ src/components/Pedidos/Pedidos.js
@@ -8,17 +8,19 @@ class Pedidos extends Component{
     render(){
         let platosPedidos = [];
         const llenarPlatosPedidos = VarTienda.platos.forEach((plato, index) => {
-            platosPedidos.push(
-                <div className="pedido" key={index}>
-                    <div className="pedido-wrapper">
-                        <h4> {plato.nombre} </h4>
-                        <div className="pedido-precio-cantidad">
-                            <span>Cantidad: {plato.cantidad}</span>
-                            <span className="plato-precio">Precio: {plato.cantidad*plato.precio}</span>
+            if(plato.cantidad != 0){
+                platosPedidos.push(
+                    <div className="pedido" key={index}>
+                        <div className="pedido-wrapper">
+                            <h4> {plato.nombre} </h4>
+                            <div className="pedido-precio-cantidad">
+                                <span>Cantidad: {plato.cantidad}</span>
+                                <span className="plato-precio">Precio: {plato.cantidad*plato.precio}</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            );
+                );
+            }
         });
         return(
             <div className="pedidos-container col-md-6">
