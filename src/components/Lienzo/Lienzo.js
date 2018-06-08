52dad03a9fe60f9b947abe9232eabc9b0eb6bc56
germanps <gpla@pasiona.com>
2018-06-08 07:47:51 +0200

    css refactor y añadir o quitar clase active en las tabs

--- src/components/Lienzo/Lienzo.js
+++ src/components/Lienzo/Lienzo.js
@@ -2,40 +2,60 @@ import React, { Component } from 'react';
 import Platos from './../Platos/Platos';
 import Bebidas from './../Bebidas/Bebidas';
 import Pedidos from './../Pedidos/Pedidos';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Redirect } from 'react-router-dom';
 import './Lienzo.css';
 
 class Lienzo extends Component{
-   render(){
+    activeLink(e){
+        const platos = document.querySelector('.tab-platos');
+        const bebidas = document.querySelector('.tab-bebidas')
+        const link = e.target;
+        if (link.classList.contains('tab-platos')) {
+            platos.classList.add('active')
+            bebidas.classList.remove('active')
+        }else{
+            bebidas.classList.add('active')
+            platos.classList.remove('active')
+        }        
+    }
+    render(){
         /* console.log(this.props.location);
         console.log(this.props.match);
         console.log(this.props.history); */
-      return( 
-         <div className="container-fluid">
-            <header className="header row">
-               <img src="./src/content/img/header.png" alt="header"/>
-            </header>
-            <div className="rest-jumbotron">
-               <h2>Restaurant</h2>
-            </div>
-            <div className="contenido-restaurante clearfix">
-              
+        return( 
+            <div className="container-fluid">
+                <header className="header row">
+                <img src="./src/content/img/header.png" alt="header"/>
+                </header>
+                <div className="rest-jumbotron">
+                <h2>Restaurant</h2>
+                </div>
+                <div className="contenido-restaurante clearfix">
+                
                     <div className="tab-container">
                         <div className="tab-wrapper">
-                            <Link to="/platos">Platos</Link>
-                            <Link to="/bebidas">Bebidas</Link>
+                            <ul className="tab-list">
+                                <li className="tab-list-item">
+                                    <Link to="/platos" className="tab-platos active" onClick={this.activeLink}>Platos</Link>
+                                </li>
+                                <li className="tab-list-item">
+                                    <Link to="/bebidas" className="tab-bebidas" onClick={this.activeLink}>Bebidas</Link>
+                                </li>
+                            </ul>
+                        
                         </div>
-                        <div>
-                            <Route path="/platos" component={Platos} />
+                        <div className="menu-wrapper">
+                            <Redirect from="/" to="/platos" />
+                            <Route exact path="/platos"  component={Platos} />
                             <Route path="/bebidas" component={Bebidas} />
                         </div>
                     </div>
-               
-               <Pedidos />  
+                
+                <Pedidos />  
+                </div>
             </div>
-         </div>
-      )
-   }
+        )
+    }
 }
 
 export default Lienzo;
---
+++ src/content/js/main.js
@@ -0,0 +1,13 @@
+/* Esta funcionalidad de añadir clase o quitarla está dispuesta en el mismo componente Lienzo */
+/* window.addEventListener('load', () => {
+   const links = document.querySelectorAll('.tab-list-item a');
+   links.forEach( (el, index) => {
+      el.addEventListener('click', (e) => {
+         const links = document.querySelectorAll('.tab-list-item a');
+         links.forEach(el => {
+            el.className = "tab";
+         });
+         e.target.className = "tab active";
+      })
+   });
+}, false); */
